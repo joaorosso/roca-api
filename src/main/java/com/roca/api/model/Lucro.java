a02b163706952e08090543bf493f855aa9c386e9
Joao Rosso <joaocarlos.rosso@thomsonreuters.com>
2020-09-02 22:06:08 -0300

    fix lucro model imports

--- src/main/java/com/roca/api/model/Lucro.java
+++ src/main/java/com/roca/api/model/Lucro.java
@@ -4,13 +4,17 @@ import com.sun.istack.NotNull;
 import lombok.Data;
 import org.hibernate.annotations.Formula;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
 @Data
 public class Lucro {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
