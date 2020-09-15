dbeb2c1ca608a52b9a98b0d514a8c4940974b5f6
Joao Rosso <joaocarlos.rosso@thomsonreuters.com>
2020-09-15 13:33:16 -0300

    create vendas report

--- src/main/java/com/roca/api/resource/LucroResource.java
+++ src/main/java/com/roca/api/resource/LucroResource.java
@@ -2,9 +2,12 @@ package com.roca.api.resource;
 
 import com.roca.api.model.Lucro;
 import com.roca.api.repository.LucroRepository;
+import com.roca.api.service.VendasService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,9 @@ public class LucroResource {
 
     @Autowired
     private LucroRepository lucroRepository;
+
+    @Autowired
+    private VendasService vendasService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -46,5 +52,15 @@ public class LucroResource {
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable UUID id) {
         lucroRepository.deleteById(id);
+    }
+
+    @GetMapping("/report/{rocaId}")
+    public ResponseEntity<byte[]> vendasReport(@PathVariable UUID rocaId) throws Exception {
+        byte[] report = vendasService.vendasReport(rocaId);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=relatorio-vendas.pdf")
+                .body(report);
     }
 }
--- src/main/java/com/roca/api/service/VendasService.java
+++ src/main/java/com/roca/api/service/VendasService.java
@@ -1,8 +1,8 @@
 package com.roca.api.service;
 
-import com.roca.api.model.Despesa;
+import com.roca.api.model.Lucro;
 import com.roca.api.model.Roca;
-import com.roca.api.repository.DespesaRepository;
+import com.roca.api.repository.LucroRepository;
 import com.roca.api.repository.RocaRepository;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -16,30 +16,34 @@ import java.math.BigDecimal;
 import java.util.*;
 
 @Service
-public class DespesaService {
+public class VendasService {
 
     @Autowired
     private RocaRepository rocaRepository;
 
     @Autowired
-    private DespesaRepository despesaRepository;
+    private LucroRepository lucroRepository;
 
-    public byte[] despesasReport(UUID rocaId) throws Exception {
+    public byte[] vendasReport(UUID rocaId) throws Exception {
         Roca roca = rocaRepository.findById(rocaId).orElse(null);
-        List<Despesa> despesas = despesaRepository.findAllByRocaId(rocaId);
+        List<Lucro> vendas = lucroRepository.findAllByRocaId(rocaId);
 
         Map<String, Object> parameters = new HashMap<>();
 
+        BigDecimal totalVendas = roca.getLucro();
         BigDecimal totalDespesa = roca.getDespesa();
+        BigDecimal totalLucro = totalVendas.subtract(totalDespesa);
 
+        parameters.put("TOTAL_VENDAS", totalVendas);
         parameters.put("TOTAL_DESPESAS", totalDespesa);
+        parameters.put("TOTAL_LUCRO", totalLucro);
         parameters.put("ROCA_DESCRICAO", roca.getDescricao());
         parameters.put("REPORT_LOCALE", new Locale("pt", "BR"));
 
         InputStream inputStream = this.getClass().getResourceAsStream(
-                "/reports/despesas.jasper");
+                "/reports/vendas.jasper");
 
-        JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parameters, new JRBeanCollectionDataSource(despesas, false));
+        JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parameters, new JRBeanCollectionDataSource(vendas, false));
 
         return JasperExportManager.exportReportToPdf(jasperPrint);
     }
